4748ce6fa992b749f08824d98e69de5cc0ceb181
gabyrei <gabriela_rei8oo@outlook.com>
2022-05-19 22:56:19 -0300

    calculadora

---
+++ ProjetoHelloWorld/src/main/kotlin/Exerc4.kt
@@ -0,0 +1,36 @@
+
+
+fun main() {
+
+    println("digite o primeiro valor e o segundo valor")
+    val num1 = readLine().toInt()
+    val num2 = readLine().toInt()
+
+    println("somando $num1 e $num2 é igual ${soma(num1, num2)}")
+
+
+
+    fun soma(int1, x int2)
+    val resultadosoma = x1 + x2
+    return resultadosoma
+}
+fun sub(int1 , x int2 )
+   val resultsub = x1 - x2
+  return resultSub
+
+        fun saida {
+
+            println("valor somado é" :${soma(num1, num2)}")
+            println(" multiplicando é:${mult(num1, num2)}")
+            println("dividindo é:${divisao(num1, num2)}")
+        }
+
+
+
+
+
+
+
+
+
+        }
