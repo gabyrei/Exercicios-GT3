bcd1546be5803044d47726e95038705aa15739b6
gabyrei <gabriela_rei8oo@outlook.com>
2022-05-23 23:05:00 -0300

    Exercicios De Class

--- ProjetoHelloWorld/src/main/kotlin/ExeAvião.java
+++ ProjetoHelloWorld/src/main/kotlin/ExeAvião.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class ExeAvião {
+public class ExeAvião  (
+        var  turbina :v  Int ,
+        var  asas :  Int ,
+        var  cor :  String ,
+
+        ){
+
+    pusar var= int(){
+        if (voando ) {
+            println("se o avião já saiu ")
+        } senão{
+            ser= verdadeiro
+                    println("o avição vai decolar)
+
+
+        }
+
+
+
+
+        }
 }
+
+
+
+
+
+
